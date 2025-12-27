3dca65a459b9c6d3ddbb5da223a8cf88b1523a42
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2025-12-27 20:54:42 +0545

    dash

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function DashboardPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-semibold text-gray-900">
+        Welcome to Dashboard
+      </h1>
+    </div>
+  );
+}
