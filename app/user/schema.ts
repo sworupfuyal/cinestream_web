5dbb4dd2117f1cdb70a751fcb0fd9d32c6493074
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2026-02-14 21:53:48 +0545

    changes to schemas

--- app/user/schema.ts
+++ app/user/schema.ts
@@ -36,3 +36,20 @@ export const updateUserSchema = z.object({
 });
 
 export type UpdateUserData = z.infer<typeof updateUserSchema>;
+
+
+export const forgetPasswordSchema = z.object({
+    email: z.email({ message: "Enter a valid email" }),
+});
+export type ForgetPasswordData = z.infer<typeof forgetPasswordSchema>;
+
+
+export const resetPasswordSchema = z.object({
+    newPassword: z.string().min(6, { message: "Minimum 6 characters" }),
+    confirmNewPassword: z.string().min(6, { message: "Minimum 6 characters" }),
+}).refine((v) => v.newPassword === v.confirmNewPassword, {
+    path: ["confirmNewPassword"],
+    message: "Passwords do not match",
+});
+
+export type ResetPasswordData = z.infer<typeof resetPasswordSchema>;
