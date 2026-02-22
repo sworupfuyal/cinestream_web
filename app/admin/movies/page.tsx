8e933f83255cd1728a75cd6755b15a1f4f811cab
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2026-02-22 13:34:25 +0545

    ADmin Add Movie Page

---
+++ app/admin/movies/page.tsx
@@ -0,0 +1,9 @@
+import MovieList from "../_components/MovieListComponent";
+
+export default function MoviesPage() {
+    return (
+        <div className="min-h-screen bg-slate-950 p-6">
+            <MovieList />
+        </div>
+    );
+}
